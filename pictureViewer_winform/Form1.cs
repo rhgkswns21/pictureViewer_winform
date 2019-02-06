13a60ab1b37ab3410aecc1276a8764c78fb12ad1
ko <ko@MTES01-42PC>
2019-02-06 19:11:29 +0900

    Project Create

--- pictureViewer_winform/Form1.cs
+++ pictureViewer_winform/Form1.cs
@@ -16,5 +16,20 @@ namespace pictureViewer_winform
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
